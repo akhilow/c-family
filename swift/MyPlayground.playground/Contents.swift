48b4366d07288257c8ec9810caacd246324b647c
AkHiLow <ryanlow1117@hotmail.com>
2020-09-07 23:20:18 -0400

    testing

---
+++ swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//import UIKit
+
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var largest_kind: String? = nil
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largest_kind = kind
+        }
+    }
+}
+print(largest_kind, largest)
+
